acd855b32bac9013334af0a0270a9a1936da1da0
mwruszczyk <marcin.ruszczyk@gazeta.pl>
2021-07-13 19:57:20 +0200

    Decorator added

---
+++ DesignPatternsRealExamples/Decorator/Decorator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsRealExamples.Decorator
+{
+    public abstract class Decorator : LibraryItem
+    {
+        protected LibraryItem libraryItem;
+
+        public Decorator(LibraryItem libraryItem)
+        {
+            this.libraryItem = libraryItem;
+        }
+
+        public override void Display()
+        {
+            libraryItem.Display();
+        }
+
+    }
+}
---
+++ DesignPatternsRealExamples/Decorator/DecoratorRun.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsRealExamples.Decorator
+{
+    class DecoratorRun
+    {
+        public DecoratorRun()
+        {
+            Book book = new Book("Worley", "Inside ASP.NET", 10);
+            book.Display();
+
+            //create video
+
+            Video video = new Video("Spielberg", "Jaws", 23, 92);
+            video.Display();
+
+            // Make video borrowable, then borrow and display
+            Console.WriteLine("\nMaking video borrowable:");
+            Borrowable borrowvideo = new Borrowable(video);
+            borrowvideo.BorrowItem("Customer #1");
+            borrowvideo.BorrowItem("Customer #2");
+            borrowvideo.Display();
+            // Wait for user
+            Console.ReadKey();
+
+        }
+    }
+}
