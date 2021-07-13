96f546dc3732f2232ff217314d97947f35a2da01
mwruszczyk <marcin.ruszczyk@gazeta.pl>
2021-07-13 20:36:56 +0200

    Facade added

---
+++ DesignPatternsRealExamples/Facade/Credit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsRealExamples.Facade
+{
+    public class Credit
+    {
+        public bool HasGoodCredit(Customer c)
+        {
+            Console.WriteLine("Check credit for " + c.Name);
+            return true;
+        }
+    }
+}
