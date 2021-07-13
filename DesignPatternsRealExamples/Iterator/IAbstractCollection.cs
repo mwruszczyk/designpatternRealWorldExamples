68e0af472d45d7861ab2a3cb09be680c8eb157a7
mwruszczyk <marcin.ruszczyk@gazeta.pl>
2021-07-13 21:45:41 +0200

    Iterator added

---
+++ DesignPatternsRealExamples/Iterator/IAbstractCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsRealExamples.Iterator
+{
+    public interface IAbstractCollection
+    {
+        Iterator CreateIterator();
+    }
+}
---
+++ DesignPatternsRealExamples/Iterator/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsRealExamples.Iterator
+{
+    public class Item
+    {
+        string name;
+        // Constructor
+        public Item(string name)
+        {
+            this.name = name;
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+    }
+}
