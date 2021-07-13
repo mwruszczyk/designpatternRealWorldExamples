acf53bb2b9605c2c08a524e08cc9c411f33d7813
mwruszczyk <marcin.ruszczyk@gazeta.pl>
2021-07-13 21:12:51 +0200

    ChainOfResponsibility

---
+++ DesignPatternsRealExamples/ChainOfResponsibility/ChainRun.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsRealExamples.ChainOfResponsibility
+{
+    class ChainRun
+    {
+        public ChainRun()
+        {
+            Approver larry = new Director();
+            Approver sam = new VicePresident();
+            Approver tammy = new President();
+            larry.SetSuccessor(sam);
+            sam.SetSuccessor(tammy);
+            // Generate and process purchase requests
+            Purchase p = new Purchase(2034, 350.00, "Supplies");
+            larry.ProcessRequest(p);
+            p = new Purchase(2035, 32590.10, "Project X");
+            larry.ProcessRequest(p);
+            p = new Purchase(2036, 122100.00, "Project Y");
+            larry.ProcessRequest(p);
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPatternsRealExamples/ChainOfResponsibility/President.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsRealExamples.ChainOfResponsibility
+{
+    public class President : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Amount < 100000.0)
+            {
+                Console.WriteLine("{0} approved request# {1}",
+                    this.GetType().Name, purchase.Number);
+            }
+            else
+            {
+                Console.WriteLine(
+                    "Request# {0} requires an executive meeting!",
+                    purchase.Number);
+            }
+        }
+    }
+}
