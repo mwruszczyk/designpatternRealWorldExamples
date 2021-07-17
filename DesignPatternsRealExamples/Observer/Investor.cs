dbafdcd518b0676f0d0b47daa101ff2a24ebff94
mwruszczyk <marcin.ruszczyk@gazeta.pl>
2021-07-17 13:14:57 +0200

    Observer added

---
+++ DesignPatternsRealExamples/Observer/Investor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsRealExamples.Observer
+{
+    public class Investor : IInvestor
+    {
+        private string name;
+        private Stock stock;
+        // Constructor
+        public Investor(string name)
+        {
+            this.name = name;
+        }
+        public void Update(Stock stock)
+        {
+            Console.WriteLine("Notified {0} of {1}'s " +
+                "change to {2:C}", name, stock.Symbol, stock.Price);
+        }
+        // Gets or sets the stock
+        public Stock Stock
+        {
+            get { return stock; }
+            set { stock = value; }
+        }
+    }
+}
