b6b2d67e1b7b11ed26d9e9ba682b0fabaf293ffe
mwruszczyk <marcin.ruszczyk@gazeta.pl>
2021-07-17 13:50:06 +0200

    TemplateMethod added

---
+++ DesignPatternsRealExamples/TemplateMethod/DataAccessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsRealExamples.TemplateMethod
+{
+    public abstract class DataAccessor
+    {
+        public abstract void Connect();
+        public abstract void Select();
+        public abstract void Process(int top);
+        public abstract void Disconnect();
+        // The 'Template Method' 
+        public void Run(int top)
+        {
+            Connect();
+            Select();
+            Process(top);
+            Disconnect();
+        }
+    }
+}
---
+++ DesignPatternsRealExamples/TemplateMethod/TemplateRun.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsRealExamples.TemplateMethod
+{
+    class TemplateRun
+    {
+        public TemplateRun()
+        {
+            DataAccessor categories = new Categories();
+            categories.Run(5);
+            DataAccessor products = new Products();
+            products.Run(3);
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
